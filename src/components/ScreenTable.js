134cc5c1dcd14d970ad6905cf1666aa39c556567
KirAve <kiryl.averkiyeu@rockwool.com>
2022-03-10 16:04:26 +0100

    add legend and minor fixes

--- src/components/ScreenTable.js
+++ src/components/ScreenTable.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, Fragment } from "react";
-import { BallTriangle } from "react-loader-spinner";
+import { TailSpin } from "react-loader-spinner";
 import { getApiAuth, fetchApiData } from "../utils/helpers";
 import { API_KEY, API_SECRET } from "../utils/constants";
 
@@ -11,7 +11,7 @@ const ScreenTable = () => {
   const [orderBook, setOrderBook] = useState();
   const [isLoading, setIsLoading] = useState(true);
   const [isInDraftMode, setIsInDraftMode] = useState(true);
-  const [totalLimit, setTotalLimit] = useState();
+  const [totalLimit, setTotalLimit] = useState("");
 
   useEffect(() => {
     getMarkets();
@@ -54,55 +54,67 @@ const ScreenTable = () => {
   const renderAsksBids = () => {
     if (orderBook && orderBook.length > 0 && !isInDraftMode) {
       let count = 0;
-      return orderBook.map((marketObj, i) => {
-        const { market, asks, bids } = marketObj;
-        const limit = totalLimit ? totalLimit : 9999999999;
+      return (
+        <Fragment>
+          <span className="item-header">Market</span>
+          <span className="item-header">Price</span>
+          <span className="item-header">Size</span>
+          <span className="item-header">Total</span>
+          {orderBook.map((marketObj, i) => {
+            const { market, asks, bids } = marketObj;
+            const limit = totalLimit ? totalLimit : 9999999999;
 
-        const asksWithinLimit = asks.map((order, j) => {
-          const total = Number((order[0] * order[1]).toFixed(3));
-          if (total > limit) {
-            const isOdd = count % 2 === 0 ? "odd" : "";
-            count++;
+            const asksWithinLimit = asks.map((order, j) => {
+              const total = Number((order[0] * order[1]).toFixed(3));
+              if (total > limit) {
+                const isOdd = count % 2 === 0 ? "odd" : "";
+                count++;
+                return (
+                  <Fragment key={j}>
+                    <span className={`item-market positive-${isOdd}`}>
+                      {market}
+                    </span>
+                    <span className={`item-price ${isOdd}`}>{order[0]}</span>
+                    <span className={`item-size ${isOdd}`}>{order[1]}</span>
+                    <span className={`item-total positive-${isOdd}`}>
+                      {total}
+                    </span>
+                  </Fragment>
+                );
+              }
+              return "";
+            });
+
+            const bidsWithinLimit = bids.map((order, j) => {
+              const total = Number((order[0] * order[1]).toFixed(3));
+              if (total > limit) {
+                const isOdd = count % 2 === 0 ? "odd" : "";
+                count++;
+                return (
+                  <Fragment key={j}>
+                    <span className={`item-market negative-${isOdd}`}>
+                      {market}
+                    </span>
+                    <span className={`item-price ${isOdd}`}>{order[0]}</span>
+                    <span className={`item-size ${isOdd}`}>{order[1]}</span>
+                    <span className={`item-total negative-${isOdd}`}>
+                      {total}
+                    </span>
+                  </Fragment>
+                );
+              }
+              return "";
+            });
+
             return (
-              <Fragment key={j}>
-                <span className={`item-market positive-${isOdd}`}>
-                  {market}
-                </span>
-                <span className={`item-price ${isOdd}`}>{order[0]}</span>
-                <span className={`item-size ${isOdd}`}>{order[1]}</span>
-                <span className={`item-total positive-${isOdd}`}>{total}</span>
+              <Fragment key={i}>
+                {asksWithinLimit}
+                {bidsWithinLimit}
               </Fragment>
             );
-          }
-          return "";
-        });
-
-        const bidsWithinLimit = bids.map((order, j) => {
-          const total = Number((order[0] * order[1]).toFixed(3));
-          if (total > limit) {
-            const isOdd = count % 2 === 0 ? "odd" : "";
-            count++;
-            return (
-              <Fragment key={j}>
-                <span className={`item-market negative-${isOdd}`}>
-                  {market}
-                </span>
-                <span className={`item-price ${isOdd}`}>{order[0]}</span>
-                <span className={`item-size ${isOdd}`}>{order[1]}</span>
-                <span className={`item-total negative-${isOdd}`}>{total}</span>
-              </Fragment>
-            );
-          }
-          return "";
-        });
-
-        return (
-          <Fragment key={i}>
-            {asksWithinLimit}
-            {bidsWithinLimit}
-          </Fragment>
-        );
-      });
+          })}
+        </Fragment>
+      );
     }
     return [];
   };
@@ -132,22 +144,34 @@ const ScreenTable = () => {
             disabled={isLoading || totalLimit > 1000000000}
             onClick={(e) => setIsInDraftMode(false)}
           >
-            Find
+            {isLoading ? (
+              <TailSpin color="#00BFFF" height={20} width={20} />
+            ) : (
+              "Find"
+            )}
           </button>
         </div>
       </div>
       {isLoading ? (
         <div className="loader">
-          <BallTriangle color="#00BFFF" height={100} width={100} />
+          <TailSpin color="#00BFFF" height={100} width={100} />
         </div>
       ) : (
-        <div className="screener-grid">
-          <span className="item-header">Market</span>
-          <span className="item-header">Price</span>
-          <span className="item-header">Size</span>
-          <span className="item-header">Total</span>
-          {renderAsksBids()}
-        </div>
+        !isInDraftMode && (
+          <Fragment>
+            <div className="ask-bid-legend">
+              <div className="legend-item">
+                <div className="circle ask-legend" />
+                <span>asks</span>
+              </div>
+              <div className="legend-item">
+                <div className="circle bid-legend" />
+                <span>bids</span>
+              </div>
+            </div>
+            <div className="screener-grid">{renderAsksBids()}</div>
+          </Fragment>
+        )
       )}
     </div>
   );
